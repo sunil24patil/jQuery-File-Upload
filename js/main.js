97245140f6c3c4993f0fddd048a0884e979c98ca
sunil24patil <patil.sushilkumar24@gmail.com>
2015-05-25 11:56:13 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ $(function () {
     $('#fileupload').fileupload({
         // Uncomment the following to send cross-domain cookies:
         //xhrFields: {withCredentials: true},
-        url: 'server/php/'
+        url: 'D:/sushil/New folder/'
     });
 
     // Enable iframe cross-domain access via redirect option:
